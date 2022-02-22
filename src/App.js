b81ae84a2ef301c26bfdc1035023bc14bc6be493
Malabya <malabya35@gmail.com>
2022-02-22 23:35:37 +0530

    Site title

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useEffect } from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -6,6 +7,11 @@ import Services from "./components/Services";
 import Footer from "./components/Footer";
 
 function App() {
+
+  useEffect(() => {
+    document.title = "Dhruba Medical"
+  }, [])
+
   return (
     <div className="app">
       <Header></Header>
